8f8364b1a013a0d388ad5948b1d5a45e6fe49a71
Md Nazmul Hossain <hossainmdnazmul268@gmail.com>
2023-05-01 23:59:14 +0600

    set up corse

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const cors = require("cors");
 const chefData = require('./data.json');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.use(cors());
 
 app.get('/chefData', (req,res)=> {
     res.send(chefData)
